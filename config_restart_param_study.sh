ffadd760a9fb9a05de45d840a37472cb3109b73e
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2022-03-04 11:51:32 +0000

    Restart at particular time

--- config_restart_param_study.sh
+++ config_restart_param_study.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-latest_snapshot=$(ls Data/*.sdf | sort | tail -n 1 | grep -Eo '[0-9]{4}')
-
-snapshot=latest_snapshot # use latest
-
-#t_end=50.0_num # End time
-#dt_snapshot=5 # specify new snapshot cadence (or comment out line below to disable)
+t_end=30.0_num # End time
+dt_snapshot=2.0 # specify new snapshot cadence (or comment out line below to disable)
 
 for folder in "$@"; do
-  cd $folder
+  pushd $folder
   sed -i -e 's/\(initial = \).*/\1IC_RESTART/' src/control.f90
+
+  latest_snapshot=$(ls Data/*.sdf | sort | tail -n 1 | grep -Eo '[0-9]{4}')
+  snapshot=$latest_snapshot # use latest
   sed -i -e 's/\(restart_snapshot = \).*/\1'${snapshot}'/' src/control.f90
-  #sed -i -e 's/\(t_end = \).*/\1'${t_end}'/' src/control.f90
-  #sed -i -e 's/\(dt_snapshots = \).*\(_num\)/\1'${dt_snapshots}'\2/' src/control.f90
+
+  sed -i -e 's/\(t_end = \).*/\1'${t_end}'/' src/control.f90
+  sed -i -e 's/\(dt_snapshots = \).*\(_num\)/\1'${dt_snapshot}'\2/' src/control.f90
   #sed -i -e 's/dump_mask(1:8) = .*/dump_mask(2:5) = .TRUE./' src/control.f90 # specify new variables to dump
-  cd ..
+  popd
 done
 
